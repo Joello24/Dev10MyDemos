5758d806f0010ec3f1be65d378fdbec60ea61351
Jason Klueber <jason.klueber@gmail.com>
2022-03-22 16:11:33 -0400

    Weather Almanac 1.0 WIP

---
+++ demos/WeatherAlmanac/WeatherAlmanac.BLL/RecordService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using WeatherAlmanac.Core.DTO;
+using WeatherAlmanac.Core.Interface;
+
+namespace WeatherAlmanac.BLL
+{
+    class RecordService : IRecordService
+    {
+        private IRecordRepository _repo;
+
+        public RecordService(IRecordRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public Result<DateRecord> Add(DateRecord record)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Result<DateRecord> Edit(DateRecord record)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Result<DateRecord> Get(DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Result<List<DateRecord>> LoadRange(DateTime start, DateTime end)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Result<DateRecord> Remove(DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- demos/WeatherAlmanac/WeatherAlmanac/Program.cs
+++ demos/WeatherAlmanac/WeatherAlmanac/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using WeatherAlmanac.BLL;
+using WeatherAlmanac.Core.DTO;
+using WeatherAlmanac.Core.Interface;
+using WeatherAlmanac.UI;
 
 namespace WeatherAlmanac
 {
@@ -6,7 +10,13 @@ namespace WeatherAlmanac
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ConsoleIO ui = new ConsoleIO();
+            MenuController menu = new MenuController(ui);
+            ApplicationMode mode = menu.Setup();  //Get test vs live mode
+            IRecordService service = RecordServiceFactory.GetRecordService(mode);
+            menu.Service = service;
+            menu.Run();    //Do the thing!
+
         }
     }
 }
